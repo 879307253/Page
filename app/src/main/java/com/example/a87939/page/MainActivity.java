0631acd871057feed23b00820cce99941be07391
879307253 <879307253@qq.com>
2019-05-29 21:23:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a87939/page/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.a87939.page;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        //fragment切换利用ViewPager
+        MyPageAdapter pageAdapter = new MyPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
